46dbf81f75feccecdc3f741e180dd07852bb4d39
Walidoux <ma.walidkorchi@icloud.com>
2024-07-09 17:51:40 +0100

    refactor(nvim/keymaps): add toggle keymap for NeoTree

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,6 @@
 -- Default keymaps: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Overriden keys should be provided in plugins/telescope.lua
 
-local map = vim.keymap.set
+local map = vim.keymap
 
-map('n', '<C-n>', '<cmd>Neotree toggle<cr>', { desc = 'Toggle NeoTree' })
+map.set('n', '<C-n>', '<cmd>Neotree toggle<cr>', { desc = 'Toggle NeoTree' })
