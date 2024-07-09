bfd42e69b0dcd64dc916ff4677561e9b4392097a
Walidoux <ma.walidkorchi@icloud.com>
2024-07-09 17:53:49 +0100

    refactor(nvim/picker): add keymaps

--- lua/plugins/picker.lua
+++ lua/plugins/picker.lua
@@ -4,9 +4,9 @@ return {
 	config = function()
 		require('icon-picker').setup({ disable_legacy_commands = true })
 		local opts = { noremap = true, silent = true }
-		vim.keymap.set('n', '<C-ie>', '<cmd>IconPickerYank emoji<cr>', opts)
-		vim.keymap.set('n', '<C-is>', '<cmd><IconPickerYank symbols<cr>', opts)
-		vim.keymap.set('n', '<C-ifa>', '<cmd>IconPickerYank alt_font<cr>', opts)
-		vim.keymap.set('n', '<C-ifn>', '<cmd>IconPickerYank nerd_font<cr>', opts)
+		vim.keymap.set('n', '<C-i-e>', '<cmd>IconPickerYank emoji<cr>', opts)
+		vim.keymap.set('n', '<C-i-s>', '<cmd><IconPickerYank symbols<cr>', opts)
+		vim.keymap.set('n', '<C-i-f-a>', '<cmd>IconPickerYank alt_font<cr>', opts)
+		vim.keymap.set('n', '<C-i-f-n>', '<cmd>IconPickerYank nerd_font<cr>', opts)
 	end,
 }
